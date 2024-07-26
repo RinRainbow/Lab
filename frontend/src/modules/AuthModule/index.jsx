3e6e15db64b2b17f40da605b2e28d19bee1aa061
Rin Kawano <b11015017@gapps.ntust.edu.tw>
2024-07-26 10:51:43 +0000

    LoginForm Transparent Visual Finished!

--- frontend/src/modules/AuthModule/index.jsx
+++ frontend/src/modules/AuthModule/index.jsx
@@ -1,12 +1,17 @@
 import useLanguage from '@/locale/useLanguage';
 
+import { useEffect } from 'react';
+
 import { Layout, Col, Divider, Typography } from 'antd';
 
 import AuthLayout from '@/layout/AuthLayout';
 import SideContent from './SideContent';
 import SelectLanguage from '@/components/SelectLanguage';
 
+import '@/myCSS/myDynamicBackground.css';
+
 import logo from '@/style/images/idurar-crm-erp.svg';
+import { redirect } from 'react-router-dom';
 
 const { Content } = Layout;
 const { Title } = Typography;
@@ -14,39 +19,104 @@ const { Title } = Typography;
 const AuthModule = ({ authContent, AUTH_TITLE }) => {
   const translate = useLanguage();
 
+  useEffect(() => {
+    const MIN_SPEED = 0.5;
+    const MAX_SPEED = 2;
+
+    function randomNumber(min, max) {
+      return Math.random() * (max - min) + min;
+    }
+
+    class Blob {
+      constructor(el) {
+        this.el = el;
+        const boundingRect = this.el.getBoundingClientRect();
+        this.size = boundingRect.width;
+        this.initialX = randomNumber(0, window.innerWidth - this.size);
+        this.initialY = randomNumber(0, window.innerHeight - this.size);
+        this.el.style.top = `${this.initialY}px`;
+        this.el.style.left = `${this.initialX}px`;
+        this.vx =
+          randomNumber(MIN_SPEED, MAX_SPEED) * (Math.random() > 0.5 ? 1 : -1);
+        this.vy =
+          randomNumber(MIN_SPEED, MAX_SPEED) * (Math.random() > 0.5 ? 1 : -1);
+        this.x = this.initialX;
+        this.y = this.initialY;
+      }
+
+      update() {
+        this.x += this.vx;
+        this.y += this.vy;
+        if (this.x >= window.innerWidth - this.size) {
+          this.x = window.innerWidth - this.size;
+          this.vx *= -1;
+        }
+        if (this.y >= window.innerHeight - this.size) {
+          this.y = window.innerHeight - this.size;
+          this.vy *= -1;
+        }
+        if (this.x <= 0) {
+          this.x = 0;
+          this.vx *= -1;
+        }
+        if (this.y <= 0) {
+          this.y = 0;
+          this.vy *= -1;
+        }
+
+        this.el.style.transform = `translate(${this.x - this.initialX}px, ${this.y - this.initialY}px)`;
+      }
+    }
+
+    function initBlobs() {
+      const blobEls = document.querySelectorAll('.blob');
+      const blobs = Array.from(blobEls).map((blobEl) => new Blob(blobEl));
+
+      function update() {
+        requestAnimationFrame(update);
+        blobs.forEach((blob) => blob.update());
+      }
+      requestAnimationFrame(update);
+    }
+
+    initBlobs();
+  }, []);
+
   return (
     <AuthLayout sideContent={<SideContent />}>
       <Content
         style={{
+          // padding: '80px 30px 30px',
           padding: '10px 20px',
+          // maxWidth: '440px',
+          // margin: '0 auto',
+          // background: 'red',
+          zIndex: 9
         }}
       >
+        <div className="blobs">
+          <div className="blob"></div>
+          <div className="blob"></div>
+          <div className="blob"></div>
+          <div className="blob"></div>
+          <div className="blob"></div>
+          <div className="blob"></div>
+          <div className="blob"></div>
+        </div>
         <SelectLanguage />
-      </Content>
-      <Content
-        style={{
-          padding: '80px 30px 30px',
-          maxWidth: '440px',
-          margin: '0 auto',
-        }}
-      >
-        <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 0 }} span={0}>
-          <img
-            src={logo}
-            alt="Logo"
-            style={{
-              margin: '-70px auto 40px',
-              display: 'block',
-            }}
-            height={63}
-            width={220}
-          />
-          <div className="space10" />
-        </Col>
 
-        
         <div className="site-layout-content">{authContent}</div>
       </Content>
+      {/* <div className="blobs">
+        <div className="blob"></div>
+        <div className="blob"></div>
+        <div className="blob"></div>
+        <div className="blob"></div>
+        <div className="blob"></div>
+        <div className="blob"></div>
+        <div className="blob"></div>
+      </div>
+      <div className="site-layout-content">{authContent}</div> */}
     </AuthLayout>
   );
 };
